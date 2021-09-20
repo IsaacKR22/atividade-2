3de75a7d8a9d55c0a797cf3b0d524535379df898
Isaac <isaac.k.rossatto@gmail.com>
2021-09-20 19:41:48 -0300

    atividade2

---
+++ salaDeAula.js
@@ -0,0 +1,8 @@
+ let salaDeAula = ["isaac","felipe","jorge","sofia","victor","klaus","lavi","maria","jeff","takumi"];
+ if(salaDeAula.length == 0){
+    console.log("Zero,ninguem,vazio,total igual : " + salaDeAula.length);
+} else if(salaDeAula.length % 2 == 0){
+    console.log("Alunos Pares,Alunos totais : " + salaDeAula.length);
+} else if(salaDeAula.length % 2 == 1){
+    console.log("Alunos Ímpares,Alunos totais : " + salaDeAula.length);
+} 
